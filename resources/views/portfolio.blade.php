222c9282767c5b8d8638af2cec01a86d8b4df172
pedromontesi <joaopedromonteiro090@hotmail.com>
2025-08-18 17:31:55 -0300

    Mudanças no tailwind

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="flex gap-8 md:gap-12 flex-col">
-        <h2 class="text-balance text-2xl md:text-5xl text-blue-100 font-bold mt-8">GitHub</h2>
+        <h2 class="text-balance text-2xl md:text-5xl text-blue-100 font-bold mt-8">Recentes</h2>
 
         <div class="gap-8 md:gap-8">
-            <ul class="rounded-3xl text-balance text-base md:text-2xl text-neutral-100 h-158 default-theme px-12 py-10 md:h-172 w-60">
+            <ul class="rounded-3xl text-balance text-base md:text-2xl text-neutral-100 default-theme px-12 py-10 h-157 md:h-171 w-60">
                 @foreach ($repos as $repo)
                     <li>
                         <a href="{{ $repo['html_url'] }}" target="_blank"
